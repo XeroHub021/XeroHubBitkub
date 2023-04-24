381e012f67453b55321ac67cafcbcda6d7ae4986
ZeroKungSoKing <131689611+XeroHub021@users.noreply.github.com>
2023-04-25 01:13:17 +0700

    Update check.php

--- Beta/check.php
+++ Beta/check.php
@@ -1,12 +1,25 @@
-local keychecked = false
+<?php
 
-if _G.Key == "ZeroKeyTEST" then
-    keychecked = true
-end
+$Key = $_GET["Key"];
+$HWID = $_GET["HWID"];
 
-if keychecked == true then
-       print("Whitelisted")
-   else
-       print("Not Whitelisted")
-   end
-end
+$KeyList = array(
+    "KEY-A",
+    "KEY-B"
+);
+
+$HWIDList = array(
+    "HELLO"
+);
+
+if (in_array($Key,$KeyList,TRUE)) {
+    if (in_array($HWID,$HWIDList,TRUE)) {
+        echo "WHITELIST !";
+    } else {
+        echo "Invaid HWID !";
+    };
+} else {
+    echo "Invaid Key";
+};
+
+?>
