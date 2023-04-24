9928901d48411bb3a6ce8aa42f059fcb2abade5b
ZeroKungSoKing <131689611+XeroHub021@users.noreply.github.com>
2023-04-24 22:36:02 +0700

    Delete check.php

--- check.php
+++
@@ -1,7 +0,0 @@
-local key = _G.Key
-local check = "https://raw.githubusercontent.com/XeroHub021/XeroHub/main/check.php?token=GHSAT0AAAAAACBZ5CFQ5GC2HA2CLREEWRB6ZCGTQHA?key=" .. key
-if game:HttpGet(check) == "Whitelisted" then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/XeroHub021/XeroHub/main/script.lua?token=GHSAT0AAAAAACBZ5CFQGNDLTMLGQBBBI5GCZCGTUOQ"))()
-else
-game.Players.LocalPlayer:Kick("คีย์ผิดคับน้อง")
-end
